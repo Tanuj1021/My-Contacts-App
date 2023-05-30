35bc2c1691670fc28795ef3835df0ab3b442ccec
Tanuj1021 <54186010+Tanuj1021@users.noreply.github.com>
2023-05-30 13:36:29 +0530

    Initial Commit

---
+++ src/components/ContactList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const ContactList = () => {
+    return (
+        <div className="ui celled list"><h1>Contact List</h1></div>
+    );
+};
+
+export default ContactList;
